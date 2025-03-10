604c63c1bc4924e69401a1a37113809bde2aadf7
priyankarc2006 <priyankarcpriya12@gmail.com>
2025-03-10 15:43:32 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+program to arithmetic operation
+a=int(input("enter a"))
+b=int(input("enter b"))
+print("addition result",a+b)
+print("division=",a/b)
+print("devision",a//b)
+print("devision",a%b)
+print("substraction",a-b)
